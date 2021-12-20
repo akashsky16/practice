2468950ffacde97d972c51f3d7601d95391566c4
akashsky16 <akash.singh1696@gmail.com>
2021-12-20 17:32:23 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("myForm");
+var btn = document.getElementById("submit");
+var span = document.getElementsByClassName("close")[0];
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
